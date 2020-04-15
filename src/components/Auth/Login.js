b4ec8fef523aec651587da098c51f3eb6efbb30d
tarwin3v <tarwin.ev@gmail.com>
2020-04-15 17:51:56 +0200

    +Login comp / HOC withRouter on index //convert Root comp in stateful comp

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -1,8 +1,98 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+//SEMANTIC UI
+import { Grid, Form, Segment, Button, Header, Message, Icon } from 'semantic-ui-react';
+//FIREBASE
+import firebase from '../../firebase';
 
 class Login extends Component {
+	state = {
+		email: '',
+		password: '',
+		errors: [],
+		loading: false
+	};
+
+	handleChange = (event) => {
+		this.setState({ [event.target.name]: event.target.value });
+	};
+	handleSubmit = (event) => {
+		event.preventDefault();
+		if (this.isFormValid(this.state)) {
+			this.setState({ errors: [], loading: true });
+			firebase
+				.auth()
+				.signInWithEmailAndPassword(this.state.email, this.state.password)
+				.then((signedInUser) => {
+					console.log(signedInUser);
+					this.setState({ loading: false });
+				})
+				.catch((err) => {
+					console.log(err);
+					this.setState({ errors: this.state.errors.concat(err), loading: false });
+				});
+		}
+	};
+
+	isFormValid = ({ email, password }) => email && password;
+
+	handleInputError = (errors, inputName) => {
+		return errors.some((error) => error.message.toLowerCase().includes(inputName)) ? 'error' : '';
+	};
+
+	displayErrors = (errors) => errors.map((error, i) => <p key={i}>{error.message}</p>);
+
 	render() {
-		return <div>Login</div>;
+		const { email, password, errors, loading } = this.state;
+		return (
+			<Grid textAlign="center" verticalAlign="middle" className="app">
+				<Grid.Column style={{ maxWidth: 450 }}>
+					<Header as="h1" icon color="orange" textAlign="center">
+						<Icon name="code branch" color="orange" />Login for DevChat
+					</Header>
+					<Form size="large" onSubmit={this.handleSubmit}>
+						<Segment stacked>
+							<Form.Input
+								type="email"
+								className={this.handleInputError(errors, 'email')}
+								fluid
+								name="email"
+								icon="mail"
+								iconPosition="left"
+								placeholder="Email adress"
+								onChange={this.handleChange}
+								value={email}
+							/>
+							<Form.Input
+								type="password"
+								className={this.handleInputError(errors, 'password')}
+								fluid
+								name="password"
+								icon="lock"
+								iconPosition="left"
+								placeholder="Password"
+								onChange={this.handleChange}
+								value={password}
+							/>
+
+							<Button
+								className={loading ? 'loading' : ''}
+								disabled={loading}
+								color="orange"
+								fluid
+								size="large"
+							>
+								Submit
+							</Button>
+						</Segment>
+					</Form>
+					{errors.length > 0 && <Message error>{this.displayErrors(errors)}</Message>}
+					<Message>
+						You dont have an account ? <Link to="/register">Register</Link>
+					</Message>
+				</Grid.Column>
+			</Grid>
+		);
 	}
 }
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, withRouter } from 'react-router-dom';
+
+//FIREBASE
+import firebase from './firebase';
 
 //STYLE
 import 'semantic-ui-css/semantic.min.css';
@@ -11,15 +14,32 @@ import App from './components/App';
 import Login from './components/Auth/Login';
 import Register from './components/Auth/Register';
 
-const Root = () => (
-	<Router>
-		<Switch>
-			<Route exact path="/" component={App} />
-			<Route path="/login" component={Login} />
-			<Route path="/register" component={Register} />
-		</Switch>
-	</Router>
-);
+class Root extends React.Component {
+	componentDidMount() {
+		firebase.auth().onAuthStateChanged((user) => {
+			if (user) {
+				this.props.history.push('/');
+			}
+		});
+	}
 
-ReactDOM.render(<Root />, document.getElementById('root'));
+	render() {
+		return (
+			<Switch>
+				<Route exact path="/" component={App} />
+				<Route path="/login" component={Login} />
+				<Route path="/register" component={Register} />
+			</Switch>
+		);
+	}
+}
+
+const RootWithAuth = withRouter(Root);
+
+ReactDOM.render(
+	<Router>
+		<RootWithAuth />
+	</Router>,
+	document.getElementById('root')
+);
 registerServiceWorker();
