ba4dfcf0d30d048270f2455c822e7d58db1f8f37
tarwin3v <tarwin.ev@gmail.com>
2020-04-16 13:48:18 +0200

    +avatar UserPanel comp

--- src/components/SidePanel/UserPanel.js
+++ src/components/SidePanel/UserPanel.js
@@ -4,7 +4,7 @@ import React, { Component } from 'react';
 import firebase from '../../firebase';
 
 //SEMANTIC
-import { Grid, Header, Icon, Dropdown } from 'semantic-ui-react';
+import { Grid, Header, Icon, Dropdown, Image } from 'semantic-ui-react';
 
 class UserPanel extends Component {
 	state = {
@@ -36,7 +36,8 @@ class UserPanel extends Component {
 	};
 
 	render() {
-		console.log(this.props.currentUser);
+		const { user } = this.state;
+
 		return (
 			<Grid style={{ background: '#4c3c4c' }}>
 				<Grid.Column>
@@ -46,14 +47,19 @@ class UserPanel extends Component {
 							<Icon name="code branch" />
 							<Header.Content>Branch</Header.Content>
 						</Header>
+						{/* User dropdown */}
+						<Header style={{ padding: '0.25em' }} as="h4" inverted>
+							<Dropdown
+								trigger={
+									<span>
+										<Image src={user.photoURL} spaced="right" avatar />
+										{user.displayName}
+									</span>
+								}
+								options={this.dropdownOptions()}
+							/>
+						</Header>
 					</Grid.Row>
-					{/* User dropdown */}
-					<Header style={{ padding: '0.25em' }} as="h4" inverted>
-						<Dropdown
-							trigger={<span>{this.state.user.displayName}</span>}
-							options={this.dropdownOptions()}
-						/>
-					</Header>
 				</Grid.Column>
 			</Grid>
 		);
