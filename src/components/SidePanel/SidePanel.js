526a09651adf65cf4043596f38b4bc9f58b7ec57
tarwin3v <tarwin.ev@gmail.com>
2020-04-16 13:56:41 +0200

    +Channels comp

--- src/components/SidePanel/SidePanel.js
+++ src/components/SidePanel/SidePanel.js
@@ -5,6 +5,7 @@ import { Menu } from 'semantic-ui-react';
 
 //COMP
 import UserPanel from './UserPanel';
+import Channels from './Channels';
 
 class SidePanel extends Component {
 	render() {
@@ -12,6 +13,7 @@ class SidePanel extends Component {
 		return (
 			<Menu size="large" inverted fixed="left" vertical style={{ background: '#4c3c4c', fontSize: '1.2rem' }}>
 				<UserPanel currentUser={currentUser} />
+				<Channels />
 			</Menu>
 		);
 	}
