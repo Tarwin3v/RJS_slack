7b75c2fe80e74958a32c80994917b649b2cb5332
tarwin3v <tarwin.ev@gmail.com>
2020-04-19 17:48:13 +0200

    +MetaPanel Accordion

--- src/components/MetaPanel/MetaPanel.js
+++ src/components/MetaPanel/MetaPanel.js
@@ -1,8 +1,47 @@
 import React, { Component } from 'react';
+//SEMANTIC
+import { Segment, Accordion, Header, Icon } from 'semantic-ui-react';
 
 class MetaPanel extends Component {
+	state = {
+		activeIndex: 0
+	};
+
+	setActiveIndex = (event, titleProps) => {
+		const { index } = titleProps;
+		const { activeIndex } = this.state;
+		const newIndex = activeIndex === index ? -1 : index;
+		this.setState({ activeIndex: newIndex });
+	};
 	render() {
-		return <div>Meta Panel</div>;
+		const { activeIndex } = this.state;
+		return (
+			<Segment>
+				<Header as="h3" attached="top">
+					About # Channel
+				</Header>
+				<Accordion styled attached="true">
+					<Accordion.Title active={activeIndex === 0} index={0} onClick={this.setActiveIndex}>
+						<Icon name="dropdown" />
+						<Icon name="info" />
+						Channel Details
+					</Accordion.Title>
+					<Accordion.Content active={activeIndex === 0}>Details</Accordion.Content>
+					<Accordion.Title active={activeIndex === 1} index={1} onClick={this.setActiveIndex}>
+						<Icon name="dropdown" />
+						<Icon name="user cirle" />
+						Top Posters
+					</Accordion.Title>
+					<Accordion.Content active={activeIndex === 1}>Posters</Accordion.Content>
+					<Accordion.Title active={activeIndex === 2} index={2} onClick={this.setActiveIndex}>
+						<Icon name="dropdown" />
+						<Icon name="pencil" />
+						Created By
+					</Accordion.Title>
+					<Accordion.Content active={activeIndex === 2}>Creator</Accordion.Content>
+				</Accordion>
+			</Segment>
+		);
 	}
 }
 
